4f1877889b354d02993280babba812e6d4143f89
drdevelop <cxd2534118085@163.com>
2022-07-04 20:16:48 +0800

    fix: Big data output console failed

--- bin/build.js
+++ bin/build.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 // eslint-disable-next-line
-const { exec } = require('child_process');
+const { spawn } = require('child_process');
 // eslint-disable-next-line
 const path = require('path');
 // eslint-disable-next-line
@@ -22,17 +22,22 @@ babelConfigFilePath = path.join(__dirname, '../babel.config.js');
 
 const outDir = options.outDir || 'dist';
 
-exec(
-  `npx babel --extensions .ts,.js ${entry} --config-file ${babelConfigFilePath} --out-dir ${outDir} `,
-  (error, stdout, stderr) => {
-    if (stdout) {
-      console.log(stdout);
-    }
-    if (stderr) {
-      console.log(stderr);
-    }
-    if (error) {
-      console.error(error.message);
-    }
+const isWindows = process.platform === 'win32';
+
+spawn(
+  'npx',
+  [
+    'babel',
+    '--extensions',
+    '.ts,.js',
+    entry,
+    '--config-file',
+    babelConfigFilePath,
+    '--out-dir',
+    outDir,
+  ],
+  {
+    stdio: 'inherit',
+    shell: isWindows,
   }
 );
--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 // eslint-disable-next-line
-const { exec } = require('child_process');
+const { spawn } = require('child_process');
 // eslint-disable-next-line
 const path = require('path');
 
@@ -9,19 +9,22 @@ const root = process.cwd();
 let entry = process.argv[2];
 entry = path.join(root, entry);
 
-babelConfigFilePath = path.join(__dirname, '../babel.config.js');
+const babelConfigFilePath = path.join(__dirname, '../babel.config.js');
 
-exec(
-  `npx babel-node --extensions .ts,.js ${entry} --config-file ${babelConfigFilePath}`,
-  (error, stdout, stderr) => {
-    if (stdout) {
-      console.log(stdout);
-    }
-    if (stderr) {
-      console.log(stderr);
-    }
-    if (error) {
-      console.error(error.message);
-    }
+const isWindows = process.platform === 'win32';
+
+spawn(
+  'npx',
+  [
+    'babel-node',
+    '--extensions',
+    '.ts,.js',
+    entry,
+    '--config-file',
+    babelConfigFilePath,
+  ],
+  {
+    stdio: 'inherit',
+    shell: isWindows,
   }
 );
